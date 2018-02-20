0bde969f3fc46460620cc42d03f353c975894954
TomasSuarezL <tomas.sl@hotmail.com>
2018-02-20 15:05:22 -0300

    deploy changes

--- app/index.js
+++ app/index.js
@@ -4,16 +4,17 @@ const PORT = process.env.PORT || 3002;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(__dirname + '/views'));
+app.use(express.static('/server/ingresos-prensa/ingresos-prensa/app/views'));
 app.use(express.static(__dirname + '/dist'));
 app.use(express.static(__dirname + '/static'));
 
 
 app.engine('handlebars', exphbs({defaultLayout: 'main', partialsDir: __dirname + '/views/partials/'}));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'handlebars');
 
 app.get('/', function(req,res){
-    res.render('main', {layout: false});
+    res.render('main.handlebars', {layout: false});
 });
 
 
